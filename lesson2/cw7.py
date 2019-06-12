22f3599ffd1b87b4ad6c23318b5654cfa61b100d
Vilya22 <roma.vilchynskyy@gmail.com>
2019-06-12 17:58:51 +0300

    Remove files

--- lesson2/cw7.py
+++ lesson2/cw7.py
@@ -1,11 +1,10 @@
 # Play the guess until user insert 0
 if __name__ == '__main__':
     num = int(input('Quess the number '))
-    av = 0
+    sum = 0
     amount = 0
     while num != 0:
         num = int(input('Nope, try again '))
-        av += num
+        sum += num
         amount += 1
-        continue
-    print('Bingo. Average {0}. Amount of numbers {1}'.format(av, amount))
+    print('Bingo. Average {0}. Amount of numbers {1}'.format(sum, amount))
--- lesson2/func1.py
+++ lesson2/func1.py
@@ -1,10 +1,14 @@
 # function that calculates the arithmetic average
+def func():
+    return sum / len(b)
+
 if __name__ == '__main__':
-    q = [56, 3, 90, 573]
-    w = int(q[0])
-    e = int(q[1])
-    r = int(q[2])
-    t = int(q[3])
-    def sum(w, e, r, t):
-        return (w + e + r + t) / 4
-    print(sum(w, e, r, t))
+    a = input('cherez probel ')
+    b = a.split(' ')
+    sum = 0
+    for i in b:
+        sum += int(i)
+    print('sum is ')
+    func()
+    
+        
